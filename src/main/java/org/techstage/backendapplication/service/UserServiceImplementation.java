dc58b907c01528cba10471be1f60de97c0f42174
riky <contoriky@gmail.com>
2024-05-07 11:17:29 +0200

    //

--- src/main/java/org/techstage/backendapplication/service/UserServiceImplementation.java
+++ src/main/java/org/techstage/backendapplication/service/UserServiceImplementation.java
@@ -1,10 +1,13 @@
 package org.techstage.backendapplication.service;
 
 import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.techstage.backendapplication.domain.Role;
 import org.techstage.backendapplication.domain.User;
 import org.techstage.backendapplication.repository.UserRepository;
+import org.techstage.backendapplication.web.dto.UserLoginDTO;
 import org.techstage.backendapplication.web.dto.UserRegistrationDTO;
 
 import java.util.List;
@@ -14,13 +17,21 @@ import java.util.List;
 @AllArgsConstructor
 public class UserServiceImplementation implements UserService {
 
+    @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     @Override
-    public User save(UserRegistrationDTO registrationDTO) {
+    public void login(UserLoginDTO loginDTO) {
+
+    }
+
+    @Override
+    public void save(UserRegistrationDTO registrationDTO) {
         var user = new User(registrationDTO.getName(), registrationDTO.getSurname(), registrationDTO.getTelephone(),
                 registrationDTO.getEmail(), registrationDTO.getPassword(), List.of(new Role("ROLE_USER")));
-
-        return userRepository.save(user);
+        userRepository.save(user);
     }
 }
